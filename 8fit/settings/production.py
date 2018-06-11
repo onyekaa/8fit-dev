6792aabd0cb2cb170a775bba755daf9d74a9cb22
Onyeka Aghanenu <onyekaag@gmail.com>
2018-06-11 12:18:55 +0100

    Modified settings

--- 8fit/settings/production.py
+++ 8fit/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
 
 DEBUG = False
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 
 try:
     from .local import *
