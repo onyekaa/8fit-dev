2c25a54cbc45d008878404366759fb2f46cf12f6
Onyeka Aghanenu <onyekaag@gmail.com>
2018-06-15 16:20:22 +0100

    Troubleshooting Heroku import

--- 8fit/settings/production.py
+++ 8fit/settings/production.py
@@ -2,14 +2,14 @@ from .base import *
 
 DEBUG = False
 
-import django_heroku
-import dj_database_url
-django_heroku.settings(locals())
-
 import os
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
+
+import django_heroku
+import dj_database_url
+django_heroku.settings(locals())
 
 
 try:
